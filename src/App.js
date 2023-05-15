4b95a768dc4cedeb24d8c40dbf93e4226e49c7e1
pJulianV <pjuliandvargas@gmail.com>
2023-05-14 22:29:04 -0500

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
-
 import Header from "./Components/Header";
+import  {temaClaro, temaOscuro} from "./Components/UI/temas"
 import Container from "./Components/Container";
 import GlobalStyle from "./GlobalStyle";
-
+import { ThemeProvider } from "styled-components";
+import { BtnTema } from "./Components/UI";
+import SwitcherTema  from "./Components/SwitcherTema"
+import { useState } from "react";
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggleTema = () => {
+    setTema((tema) => !tema)
+  }
   return (
-    <>
+    <ThemeProvider theme={tema ? temaClaro : temaOscuro}>
       <GlobalStyle />
+      <BtnTema onClick={toggleTema}>
+        <SwitcherTema tema={true} />
+      </BtnTema>
       <Header />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
